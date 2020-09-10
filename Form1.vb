60ba66c7cec30652feccdcefbc8de8851acffd03
itotono <35085212+itotono@users.noreply.github.com>
2020-09-10 12:46:21 +0900

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class frmShowMessage
+    Private Sub ShowMassage(sender As Object, e As EventArgs) Handles btnMassage.Click
+        lblMessage.Text = "Hello VB!"
+    End Sub
+
+    Private Sub ExitProc(sender As Object, e As EventArgs) Handles btnExit.Click
+        Application.Exit()
+    End Sub
+End Class
